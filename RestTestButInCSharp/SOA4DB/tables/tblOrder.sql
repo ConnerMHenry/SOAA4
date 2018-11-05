ed9ecdbbaadaf908f6fa6f93dcc8315e5e8d8ee3
Michael Maxner <mmaxner@rogers.com>
2018-11-05 12:54:48 -0500

    Added one database

---
+++ RestTestButInCSharp/SOA4DB/tables/tblOrder.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[tblOrder]
+(
+	[orderId] INT IDENTITY(1,1) PRIMARY KEY,
+	[custId] INT NOT NULL FOREIGN KEY REFERENCES tblCustomer(custId),
+	[orderDate] DATETIME NOT NULL,
+	[poNumber] TEXT
+)
