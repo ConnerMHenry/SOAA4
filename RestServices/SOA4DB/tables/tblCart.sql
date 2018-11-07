65b8a05b0578dd4beb7a65e88b5431ced4f31fba
Michael Maxner <mmaxner@rogers.com>
2018-11-07 10:05:07 -0500

    Added DB project

---
+++ RestServices/SOA4DB/tables/tblCart.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[tblCart]
+(
+	[orderId] INT NOT NULL FOREIGN KEY REFERENCES tblOrder(orderId),
+	[prodId] INT NOT NULL FOREIGN KEY REFERENCES tblProduct(prodId),
+	[quantity] INT NOT NULL,
+	CONSTRAINT PK_Cart PRIMARY KEY (orderId, prodId)
+)
---
+++ RestServices/SOA4DB/tables/tblCustomer.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[tblCustomer]
+(
+	[custId] INT IDENTITY(1,1) PRIMARY KEY,
+	[firstName] TEXT NOT NULL,
+	[lastName] TEXT NOT NULL,
+	[phoneNumber] TEXT NOT NULL
+)
