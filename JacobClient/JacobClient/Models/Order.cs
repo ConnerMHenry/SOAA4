18e1553e87c25cc27827420de260d78f21c1482a
Jacob <pumear0@gmail.com>
2018-11-12 13:52:33 -0500

    base new client app

---
+++ JacobClient/JacobClient/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JacobClient.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public string PONumber { get; set; }
+        public DateTime OrderDate { get; set; }
+
+    }
+}
---
+++ JacobClient/JacobClient/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JacobClient.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public float Weight { get; set; }
+        public bool IsInStock { get; set; }
+    }
+}
