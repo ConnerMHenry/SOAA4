59d3c5097387e6591fe1183b5fd2810bc5010eef
Jacob <pumear0@gmail.com>
2018-11-13 15:56:54 -0500

    added testing access to controller, better forms

--- JacobClient/JacobClient/Controllers/HomeController.cs
+++ JacobClient/JacobClient/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using JacobClient.Models;
 
 namespace JacobClient.Controllers
 {
@@ -13,24 +14,80 @@ namespace JacobClient.Controllers
             return View();
         }
 
+        [HttpGet]
         public ActionResult Search()
         {
             return View();
         }
 
+        [HttpGet]
         public ActionResult Insert()
         {
             return View();
         }
 
+        [HttpGet]
         public ActionResult Update()
         {
             return View();
         }
 
+        [HttpGet]
         public ActionResult Delete()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetTestProduct()
+        {
+            Product product = new Product
+            {
+                Id = 123,
+                Name = "Water balloon",
+                Price = 1.25f,
+                Weight = 2f,
+                IsInStock = true
+            };
+            return PartialView("_CardViewProduct", product);
+        }
+
+        [HttpGet]
+        public ActionResult GetTestOrder()
+        {
+            Order order = new Order
+            {
+                CustomerId = 1,
+                Id = 54,
+                OrderDate = DateTime.Now,
+                PONumber = "ABC123"
+            };
+            return PartialView("_CardViewOrder", order);
+        }
+
+        [HttpGet]
+        public ActionResult GetTestCustomer()
+        {
+            Customer customer = new Customer
+            {
+                FirstName = "Jacob",
+                LastName = "Huras",
+                PhoneNumber = "226226575",
+                Id = 1
+            };
+            return PartialView("_CardViewCustomer", customer);
+        }
+
+        [HttpGet]
+        public ActionResult GetTestCart()
+        {
+            Cart cart = new Cart
+            {
+                OrderId = 54,
+                ProductId = 12,
+                Quantity = 2
+            };
+            return PartialView("_CardViewCart", cart);
         }
     }
 }
